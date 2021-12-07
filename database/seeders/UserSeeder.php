15eff96caf7718de191c035298dbf0bc8df6d21e
junaidi0910 <junaidi@si.ukdw.ac.id>
2021-12-07 12:05:47 +0700

    initial

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,28 +16,28 @@ class UserSeeder extends Seeder
     {
         $users = [
             [
-                'username' => 'katonwijana',
-                'nama' => 'Katon Wijana',
-                'nomor_induk' => 119022,
+                'username' => 'dosenA',
+                'nama' => 'Dosen A',
+                'nomor_induk' => 111200,
                 'np_hp' => '081315927777',
                 'email' => 'dosen@gmail.com',
                 'password' => bcrypt(123456),
                 'role' => 'dosen'
             ],
             [
-                'username' => 'anto123',
-                'nama' => 'Aldi Widodo',
-                'nomor_induk' => 119022,
+                'username' => 'mahasiswaA',
+                'nama' => 'Mahasiswa A',
+                'nomor_induk' => 72190000,
                 'np_hp' => '08954121111',
                 'email' => 'mahasiswa@gmail.com',
                 'password' => bcrypt(123456),
                 'role' => 'mahasiswa'
             ],
             [
-                'username' => 'andi123',
-                'nama' => 'Aldi Widodo',
-                'nomor_induk' => 119022,
-                'np_hp' => '08954121111',
+                'username' => 'adminA',
+                'nama' => 'Admin A',
+                'nomor_induk' => 001,
+                'np_hp' => '081345629756',
                 'email' => 'ppa@gmail.com',
                 'password' => bcrypt(123456),
                 'role' => 'ppa'
